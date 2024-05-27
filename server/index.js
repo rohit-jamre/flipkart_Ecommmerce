2011b6f15267a5dd9b194aa0bb8c83b63f86f7a5
Rohit Jamre <jamrer193@gmail.com>
2024-05-27 12:42:15 +0530

    deploy server on github

--- server/index.js
+++ server/index.js
@@ -23,3 +23,42 @@ Connection(USERNAME,PASSWORD);
  app.listen(PORT, () => console.log (`Server is running succesfully on port ${PORT}`));
  DefaultData();
  
+ /*import express from 'express';
+ import dotenv from 'dotenv';
+ import bodyParser from 'body-parser';
+ import cors from 'cors';
+ 
+ import Connection from './database/db.js';
+ import DefaultData from './default.js';
+ import Router from './routes/route.js';
+ 
+ const app = express();
+ dotenv.config();
+ 
+ // Middleware
+ app.use(cors());
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({ extended: true }));
+ 
+ // Routes
+ app.use('/', Router);
+ 
+ const PORT = process.env.PORT || 8000; // Allow dynamic port binding
+ 
+ const USERNAME = process.env.DB_USERNAME;
+ const PASSWORD = process.env.DB_PASSWORD;
+ 
+ async function startServer() {
+     try {
+         await Connection(USERNAME, PASSWORD);
+         app.listen(PORT, () => console.log(`Server is running successfully on port ${PORT}`));
+         DefaultData(); // Consider if this should be awaited or if it's a non-blocking operation
+     } catch (error) {
+         console.error('Error starting server:', error);
+         // Properly handle error, could be logging, notifying, or crashing the server depending on severity
+     }
+ }
+ 
+ startServer();
+ */
+ 
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,7 +1,11 @@
 import express from 'express';
-import {userSignup} from '../controller/user-controller.js';
-const router = express.Router();
-router.post('/signup', ()=> {
+import { userSignup,userLogin } from '../Controller/user-controller.js';
+import {getProducts,getProductById} from '../Controller/product-controller.js';
+const  router = express.Router();
 
-});
+
+router.post('/signupUser', userSignup);
+router.post('/login', userLogin);
+router.get('/products',getProducts);
+router.get('/product/:id', getProductById)
 export default router;
